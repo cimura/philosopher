7aa23a503461813b31675064e4c25b519ba7b07a
Your Name <you@example.com>
2024-09-28 10:53:08 +0900

    printする前に少しsleepしてdeath watchした

--- srcs/party.c
+++ srcs/party.c
@@ -36,16 +36,16 @@ static void	eating(t_philo *philos)
   philos->last_mealtime = gettime_ms() - philos->table->start_time;
   ft_mutex(&philos->meal_monitor, UNLOCK);
 
-  // if (philos->philo_id % 2 == 0)
-  //  {
-  //   ft_mutex(&philos->table->forks[philos->left_fork_id], UNLOCK);
-  //   ft_mutex(&philos->table->forks[philos->right_fork_id], UNLOCK);
-  //  }
-  // else
-  //  {
+  if (philos->philo_id % 2 == 0)
+   {
+    ft_mutex(&philos->table->forks[philos->left_fork_id], UNLOCK);
+    ft_mutex(&philos->table->forks[philos->right_fork_id], UNLOCK);
+   }
+  else
+   {
   	ft_mutex(&philos->table->forks[philos->right_fork_id], UNLOCK);
     ft_mutex(&philos->table->forks[philos->left_fork_id], UNLOCK);
-  //  }
+   }
 }
 
 static void thinking(t_philo *philos)
